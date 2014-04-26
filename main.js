bc27c58a6a67b89e3788a881b4a014727a2f23ae
Yahkob <jakobharclerode@gmail.com>
2014-04-26 02:06:25 -0700

    Code refactoring and commenting

--- main.js
+++ main.js
@@ -1,64 +1,56 @@
 $(document).ready(function(){
   var count = streams.home.length - 1;
-  $("#tweetbox-single").hide()
-  //get default tweets
-var renderTweets = function(){
-  for(var i = count; i >= 0; i-- ){
-    var tweet = streams.home[i];
-    var user = $('<div class="panel-heading"><a class="users" href="#"></a></div>');
-    var post = $('<div class="panel-body"></div>');
-    var time = $('<span class="text-muted" id="timestamp" data-livestamp="'+ tweet.created_at +'"></span>');
-    user.children().text("@" + tweet.user);
-    post.text(tweet.message);
-    $("#tweetbox-all").append(user);
-    $('#tweetbox-all').append(time)
-    $("#tweetbox-all").append(post);
-  }
-}
-renderTweets()
-setInterval(function(){
-    var currentCount = streams.home.length - 1;
-    if(currentCount > count){
-      for(var i = count + 1; i < currentCount; i++){
-        var tweet = streams.home[i]
-        var user = $('<div class="panel-heading"><a class="users" href="#"></a></div>');
-        var post = $('<div class="panel-body"></div>');
-        var time = $('<span class="text-muted" id="timestamp" data-livestamp="'+ tweet.created_at +'"></span>');
-        user.children().text("@" + tweet.user);
-        post.text(tweet.message)
-        $('#tweetbox-all').prepend(post);
-        $('#tweetbox-all').prepend(time)
-        $('#tweetbox-all').prepend(user);
-      }
-      count = currentCount;
-    }
-  },4000);
-  var displayUserTweets = function(user){
-    var location = streams.users[user]
-    for(var i = 0; i < location.length; i++){
-      var tweet = location[i];
+  var allTweets = $('#tweetbox-all')
+  var individualTweets = $('#tweetbox-user')
+  //hide single twittler users box on start up
+  individualTweets.hide()
+
+  //tweet retrieval function
+  var fetchTweets = function(start,end,data,location){
+    for(var i = start; i < end; i++){
+      var tweet = data[i]
       var user = $('<div class="panel-heading"><a class="users" href="#"></a></div>');
       var post = $('<div class="panel-body"></div>');
+      //'Posted about x time ago'
       var time = $('<span class="text-muted" id="timestamp" data-livestamp="'+ tweet.created_at +'"></span>');
       user.children().text("@" + tweet.user);
       post.text(tweet.message)
-      $('#tweetbox-single').prepend(post);
-      $('#tweetbox-single').prepend(time)
-      $('#tweetbox-single').prepend(user);
-      }
-  }
+      location.prepend(post);
+      location.prepend(time)
+      location.prepend(user);
+    }
+  };
+
+  //get default tweets
+  fetchTweets(0,count + 1,streams.home,allTweets)
+
+  //refreshes twittler feed every four seconds
+  setInterval(function(){
+    var currentCount = streams.home.length;
+    if(currentCount > count){
+      fetchTweets(count + 1,currentCount,streams.home,allTweets)
+      count = currentCount;
+    }
+  },4000);
+
+  //handles user click event
   $("#tweetbox-all").on("click",".users",function(){
+    //hides all tweets
     $("#tweetbox-all").hide()
-    $("#tweetbox-single").show()
+    //shows @user's tweets
+    $("#tweetbox-user").show()
     var singleUser = ($(this).text().split("").slice(1).join(""))
-    $(".navInfo").text($(this).text() +"'s tweets").append("<br><br><a id='back' href='#'>View all tweets</a>")
-    displayUserTweets(singleUser)
+    var userData = streams.users[singleUser]
+    console.log(userData[0])
+    $(".navInfo").text($(this).text() +"'s tweets").append("<br><br><a id='allTweets' href='#'>View all tweets</a>")
+    fetchTweets(0,userData.length,userData,individualTweets);
   })
-  $("#infoBox").on("click","#back",function(){
-    $("#tweetbox-single").children().remove();
+
+  //navigates to twittler feed
+  $("#infoBox").on("click","#allTweets",function(){
+    $("#tweetbox-user").children().remove();
     $("#tweetbox-all").show();
     $(".navInfo").text("All tweets")
   })
 });
-
 
